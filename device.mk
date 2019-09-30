291d0bed4ee525c2de8629e1934a157adc758fb9
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-30 16:28:44 -0400

    vayu: Build vendor variant of libdng_sdk from source
     * As per google https://android.googlesource.com/platform/external/dng_sdk/+/refs/tags/android-10.0.0_r3/Android.bp#103.
     * Silent the following:
       build/make/core/Makefile:28: warning: overriding commands for target `out/target/product/davinci/vendor/lib64/libdng_sdk.so'
       build/make/core/base_rules.mk:480: warning: ignoring old commands for target `out/target/product/davinci/vendor/lib64/libdng_sdk.so'.
    
    Change-Id: I4c909c66ab75f8ff0997844d83d3f1519b085846

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
+    libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
