9b68aef3a059ce82a5845cfce9e5cd348726af22
klozz <klozz@TheXPerienceProject.org>
2021-05-26 06:30:25 +0200

    vayu: Initial bring for xperience
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/xperience_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    xperience_vayu-eng \
+    xperience_vayu-userdebug \
+    xperience_vayu-user
--- xperience_vayu.mk
+++ xperience_vayu.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common XPerience configurations
+$(call inherit-product, vendor/xperience/config/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := xperience_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -24,7 +24,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="vayu"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-DEVICE_MAINTAINER := kubersharma001
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
