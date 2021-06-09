7d9c82c30190c947d95f3a650ae44253f748c773
kubersharma001 <kubersharma001@gmail.com>
2021-06-09 23:13:23 +0000

    vayu: Import public libraries list from stock
    Change-Id: Ied3118bfc0fea699191638f3698c78dd187a63ad
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- device.mk
+++ device.mk
@@ -280,6 +280,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.vayu
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Releasetool scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/device_nfc_check.sh:install/bin/device_nfc_check.sh
