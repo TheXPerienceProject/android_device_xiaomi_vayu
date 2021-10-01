658690146a4eb7dbf3f0a92d4406498b464bed31
kubersharma001 <kubersharma001@gmail.com>
2021-10-01 14:33:04 +0000

    vayu: Set build fp,desc at build time
    Change-Id: I1674d68a9b9b8611d75212a2d2dfeb5c3f9df3a9
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- xperience_vayu.mk
+++ xperience_vayu.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2021 The XPerience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -27,6 +28,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.4.0.RJUMIXM release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
-BUILD_FINGERPRINT := POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.4.0.RJUMIXM:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
