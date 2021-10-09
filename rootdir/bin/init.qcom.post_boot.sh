f9be6bfc473128696d630c5d675c3de0f507dbde
maitreya29 <maitreyapatni30@gmail.com>
2021-10-09 15:03:31 +0530

    vayu: Nuke storage.xml post boot
    * Workaround for TWRP splash stuck issue with android12
    
    Change-Id: I9e44564bd46a439c64b7fe2b221a20c84bc9b32d

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -6168,3 +6168,6 @@ esac
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
+rm -rf data/system/storage.xml
+touch data/system/storage.xml
+chattr +I data/system/storage.xml
