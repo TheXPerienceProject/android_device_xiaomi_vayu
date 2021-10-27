2b3bb136502bf351a58e7c8fed2a2990b15c045d
klozz <klozz@thexperienceproject.org>
2021-10-27 11:27:27 +0200

    Revert "Revert "Revert "vayu: Nuke storage.xml post boot"""
    This reverts commit 5d71d675e2d4787fa502754b098f17e539b56625.

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -6168,6 +6168,3 @@ esac
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
-rm -rf data/system/storage.xml
-touch data/system/storage.xml
-chattr +I data/system/storage.xml
