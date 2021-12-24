672edf68cbeca397e4134c704e0a7f44592b24e9
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2021-12-23 23:04:14 -0600

    vayu: call the ims symlink
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- Android.mk
+++ Android.mk
@@ -158,4 +158,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(EGL_64_SYMLINK) $(GLESv2_64_SYMLINK) $(Q3DTOO
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 ALL_DEFAULT_INSTALLED_MODULES += $(WIFI_FIRMWARE_SYMLINKS)
 
+$(shell mkdir -p $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/ && pushd $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64 > /dev/null && ln -s /system_ext/lib64/libimscamera_jni.so libimscamera_jni.so && popd > /dev/null)
+$(shell mkdir -p $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/ && pushd $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64 > /dev/null && ln -s /system_ext/lib64/libimsmedia_jni.so libimsmedia_jni.so && popd > /dev/null)
+
 endif
